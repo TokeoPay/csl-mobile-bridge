ae9989582859dead38057b4f0e216b33bc80f6b8
Gavin Harris <gavin@evie.digital>
2025-07-29 10:48:42 +1000

    Bump version to v0.0.1-alpha.5

--- CSLKit/Package.swift
+++ CSLKit/Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 import CompilerPluginSupport
 
-let useLocalBinary = true
+let useLocalBinary = false
 let version = "0.1.0"
-let haskellShelleyBinaryVersionURL = "https://github.com/TokeoPay/csl-mobile-bridge/releases/download/v0.0.1-alpha.4/react_native_haskell_shelley.artifactbundle.zip"
-let haskellShelleyBinaryChecksum = "376b0f3f18ff0391ed50e9873add97c559c457d3c2d2b0f1d2430c84a7fca306"
+let haskellShelleyBinaryVersionURL = "https://github.com/TokeoPay/csl-mobile-bridge/releases/download/v0.0.1-alpha.5/react_native_haskell_shelley.artifactbundle.zip"
+let haskellShelleyBinaryChecksum = "a3d0eb4d1f8796a9a0600db7da8bea6f46c0d0fcd69742d438a6640a12b21040"
 
 
 let package = Package(
